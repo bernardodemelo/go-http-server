84372b7a2adced63ae8953ec6c8c539423d780a6
bernardo <bernardodemelo@unknown62f6cb0036a9.lan>
2025-06-24 16:13:59 +0100

    added ent and postgresql docker image

--- main.go
+++ main.go
@@ -1,48 +1,51 @@
 package main
 
 import (
- "context"
- "fmt"
- "log"
- "os"
- "os/signal"
- "syscall"
- "time"
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
- "http-go/routes"
- "http-go/server"
+	"http-go/db"
+	"http-go/routes"
+	"http-go/server"
 )
 
 func main() {
+	client := db.Connect()
+	db.Seed(client)
 
- /* Register routes */
- router := routes.RegisterRoutes()
+	/* Register routes */
+	router := routes.RegisterRoutes()
 
- s := server.NewServer(8080, router)
+	s := server.NewServer(8080, router)
 
- // Set up graceful shutdown
- stop := make(chan os.Signal, 1)
- signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+	// Set up graceful shutdown
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
 
- go func() {
-  if err := s.Run(); err != nil {
-   log.Fatalf("Error starting server: %v", err)
-  }
- }()
+	go func() {
+		if err := s.Run(); err != nil {
+			log.Fatalf("Error starting server: %v", err)
+		}
+	}()
 
- fmt.Println("Server is running...")
+	fmt.Println("Server is running...")
 
- //Interrupt signal
- <-stop
+	//Interrupt signal
+	<-stop
 
- fmt.Println("Shutting down server...")
+	fmt.Println("Shutting down server...")
 
- ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
- defer cancel()
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
- if err := s.Shutdown(ctx); err != nil {
-  log.Fatalf("Server forced to shutdown: %v", err)
- }
+	if err := s.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %v", err)
+	}
 
- fmt.Println("Server gracefully stopped")
+	fmt.Println("Server gracefully stopped")
 }
